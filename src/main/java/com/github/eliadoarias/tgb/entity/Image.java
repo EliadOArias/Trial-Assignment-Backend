d0570ae6c06f7aa935d60c91d922e6640172902e
EliadOArias <2144359102@qq.com>
2025-09-28 16:21:36 +0800

    功能：图片操作
    ### 描述
    - 添加图片上传接口
    - 上传时自动去重
    - 添加静态资源访问

---
+++ src/main/java/com/github/eliadoarias/tgb/entity/Image.java
@@ -0,0 +1,64 @@
+package com.github.eliadoarias.tgb.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @TableName image
+ */
+@TableName(value ="image")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Image {
+    @TableId(type =  IdType.AUTO)
+    private Integer id;
+
+    private String imageUrl;
+
+    private String md5Code;
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Image other = (Image) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getImageUrl() == null ? other.getImageUrl() == null : this.getImageUrl().equals(other.getImageUrl()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getImageUrl() == null) ? 0 : getImageUrl().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", imageUrl=").append(imageUrl);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/github/eliadoarias/tgb/mapper/ImageMapper.java
@@ -0,0 +1,18 @@
+package com.github.eliadoarias.tgb.mapper;
+
+import com.github.eliadoarias.tgb.entity.Image;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author EArias
+* @description 针对表【image】的数据库操作Mapper
+* @createDate 2025-09-27 11:54:12
+* @Entity com.github.eliadoarias.tgb.entity.Image
+*/
+public interface ImageMapper extends BaseMapper<Image> {
+
+}
+
+
+
+
